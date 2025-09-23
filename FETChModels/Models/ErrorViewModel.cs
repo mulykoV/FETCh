8520bea17ae046683540c77c0527682ee02e51ab
Miha8g8g <mixail.xrapach@gmail.com>
2025-09-23 12:59:00 +0300

    додав usingв моделі та прописав DbContext

--- FETChModels/Models/ErrorViewModel.cs
+++ FETChModels/Models/ErrorViewModel.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace FETCh.Models
 {
     public class ErrorViewModel
