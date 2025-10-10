46d07447e92c2e6f746f2915ae62451f49eafe7b
Nazar Pugach <qwertyasd0933@gmail.com>
2025-10-10 12:04:10 +0300

    temp fix

--- FetchData/Repositories/BaseSQLServrRepository.cs
+++ FetchData/Repositories/BaseSQLServrRepository.cs
@@ -12,17 +12,16 @@ using System.Threading.Tasks;
 
 namespace FetchData.Repositories
 {
-     public class BaseSQLServrRepository<TDbContext> : IRepository
-        where TDbContext : DbContext
+    public class BaseSQLServrRepository<TDbContext> : IRepository
+   where TDbContext : DbContext
     {
-        private DbContext db;
+        protected readonly TDbContext Db;
 
         public BaseSQLServrRepository(TDbContext db)
         {
-            this.db = db;
+            Db = db ?? throw new ArgumentNullException(nameof(db));
         }
 
-        protected TDbContext Db { get; set;}
         public async Task<int> AddAsync<T>(T item) where T : class
         {
             await Db.Set<T>().AddAsync(item);
@@ -31,19 +30,18 @@ namespace FetchData.Repositories
 
         public IQueryable<T> All<T>() where T : class
         {
-            return Db.Model.FindEntityType(typeof(T)) != null
-                ? Db.Set<T>().AsQueryable()
-                : new Collection<T>().AsQueryable();
-        }
-
-        public async Task<T?> FirstOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class
-        {
-            return await All<T>().FirstOrDefaultAsync(expression);
+            // просто повертаємо Db.Set<T>()
+            return Db.Set<T>();
         }
 
         public IQueryable<T> ReadAll<T>() where T : class
         {
             return All<T>().AsNoTracking();
+        }
+
+        public async Task<T?> FirstOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class
+        {
+            return await ReadAll<T>().FirstOrDefaultAsync(expression);
         }
 
         public async Task<T?> ReadSingleAsync<T>(Expression<Func<T, bool>> expression) where T : class
@@ -53,7 +51,7 @@ namespace FetchData.Repositories
 
         public async Task<bool> ExistsAsync<T>(Expression<Func<T, bool>> expression) where T : class
         {
-            return await All<T>().AnyAsync(expression);
+            return await ReadAll<T>().AnyAsync(expression);
         }
 
         public IQueryable<T> ReadWhere<T>(Expression<Func<T, bool>> expression) where T : class
@@ -63,39 +61,24 @@ namespace FetchData.Repositories
 
         public async Task<int> RemoveAsync<T>(T item) where T : class
         {
-            Db.Remove(item);
-
+            Db.Set<T>().Remove(item);
             return await Db.SaveChangesAsync();
         }
 
         public async Task<T?> SingleAsync<T>(Expression<Func<T, bool>> expression) where T : class
         {
-            return await All<T>().SingleOrDefaultAsync(expression);
+            return await ReadAll<T>().SingleOrDefaultAsync(expression);
         }
 
         public async Task<int> UpdateAsync<T>(T item) where T : class
         {
-            var local = Db.Set<T>()
-                .Local
-                .FirstOrDefault(entry => entry == item);
+            var local = Db.Set<T>().Local.FirstOrDefault(entry => entry == item);
 
-            if(local != null)
-            {
+            if (local != null)
                 Db.Entry(local).State = EntityState.Detached;
-            }
-            else
-            {
-                Db.Entry(item).State = EntityState.Modified;
-            }
 
             Db.Update(item);
-
             return await Db.SaveChangesAsync();
-        }
-
-        IQueryable<T> IRepository.ReadWhere<T>(Expression<Func<T, bool>> expression) where T : class
-        {
-            throw new NotImplementedException();
         }
     }
 }
